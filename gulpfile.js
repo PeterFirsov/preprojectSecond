f94aeef36cfc2c4539e676e1abd04065a6fdc9e1
PeterFirsov <pvfirsov15@yandex.ru>
2019-12-04 01:55:24 +0300

    Преимущества, Акции, Отзывы

--- gulpfile.js
+++ gulpfile.js
@@ -1,104 +1,162 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var plumber = require("gulp-plumber");
-var sourcemap = require("gulp-sourcemaps");
-var sass = require("gulp-sass");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-var csso = require("gulp-csso");
-var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore")
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var del = require("del");
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sourcemap = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync').create();
+var csso = require('gulp-csso');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var webp = require('gulp-webp');
+var svgstore = require('gulp-svgstore')
+var posthtml = require('gulp-posthtml');
+var include = require('posthtml-include');
+var del = require('del');
+var webpack = require(`webpack-stream`);
 
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(postcss([ autoprefixer() ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
+let isDev = true;
+
+var mainWebpackConfig = {
+  output: {
+    filename: `vendor.js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: `babel-loader`,
+        exclude: [/node_modules\/(?!(swiper|dom7)\/).*/],
+        options: {
+          presets: [[`@babel/preset-env`, {
+            "targets": `> 0.25%, not dead`
+          }]]
+        }
+      }
+    ]
+  },
+  mode: isDev ? `development` : `production`,
+  devtool: isDev ? `eval-sourse-map` : `none`
+};
+
+var vendorWebpackConfig = {
+  output: {
+    filename: `script.js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [/node_modules\/(?!(swiper|dom7)\/).*/],
+        loader: `babel-loader`,
+        options: {
+          presets: [`@babel/preset-env`]
+        }
+      }
+    ]
+  },
+  mode: isDev ? `development` : `production`,
+  devtool: isDev ? `eval-sourse-map` : `none`
+};
+
+gulp.task(`main`, function () {
+  return gulp
+      .src(`source/js/script.js`)
+      .pipe(webpack(mainWebpackConfig))
+      .pipe(gulp.dest(`build/js/`));
 });
 
-gulp.task("server", function () {
+gulp.task(`vendor`, function () {
+  return gulp
+      .src(`source/js/vendor.js`)
+      .pipe(webpack(vendorWebpackConfig))
+      .pipe(gulp.dest(`build/js/`));
+});
+
+gulp.task('css', function () {
+  return gulp.src('source/sass/style.scss')
+      .pipe(plumber())
+      .pipe(sourcemap.init())
+      .pipe(sass())
+      .pipe(postcss([ autoprefixer() ]))
+      .pipe(gulp.dest('build/css'))
+      .pipe(csso())
+      .pipe(rename('style.min.css'))
+      .pipe(sourcemap.write('.'))
+      .pipe(gulp.dest('build/css'))
+      .pipe(server.stream());
+});
+
+gulp.task('server', function () {
   server.init({
-    server: "build/",
+    server: 'build/',
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/js/**.{js}", gulp.series("copy"));
-  gulp.watch("source/img/**.{png,jpg,svg}", gulp.series("copy"));
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch(`source/js/script.js`, gulp.series(`main`, `refresh`));
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
+  gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
+  gulp.watch('source/*.html', gulp.series('html', 'refresh'));
 });
 
-gulp.task("refresh", function (done) {
+gulp.task('refresh', function (done) {
   server.reload();
   done();
 });
 
-gulp.task("images", function() {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
+gulp.task('images', function () {
+  return gulp.src('source/img/**/*.{png,jpg,svg}')
+      .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.svgo()
+      ]))
 
-    .pipe(gulp.dest("source/img"));
+      .pipe(gulp.dest('source/img'));
 
 });
 
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
+gulp.task('webp', function () {
+  return gulp.src('source/img/**/*.{png,jpg}')
+      .pipe(webp({quality: 90}))
+      .pipe(gulp.dest('source/img'));
 });
 
-gulp.task("sprite", function () {
-  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
-    .pipe(svgstore({inlineSvg: true}))
-    .pipe(rename("sprite_auto.svg"))
-    .pipe(gulp.dest("build/img"));
+gulp.task('sprite', function () {
+  return gulp.src('source/img/{icon-*,htmlacademy*}.svg')
+      .pipe(svgstore({inlineSvg: true}))
+      .pipe(rename('sprite_auto.svg'))
+      .pipe(gulp.dest('build/img'));
 });
 
-gulp.task("html", function () {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"));
+gulp.task('html', function () {
+  return gulp.src('source/*.html')
+      .pipe(posthtml([
+        include()
+      ]))
+      .pipe(gulp.dest('build'));
 });
 
-gulp.task("copy", function () {
+gulp.task('copy', function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**",
-    "source//*.ico"
-    ], {
-      base: "source"
-    })
-  .pipe(gulp.dest("build"));
+    'source/fonts/**/*.{woff,woff2}',
+    'source/img/**',
+    'source/js/**',
+    'source//*.ico'
+  ], {
+    base: 'source'
+  })
+      .pipe(gulp.dest('build'));
 });
 
-gulp.task("clean", function () {
-  return del("build");
+gulp.task('clean', function () {
+  return del('build');
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
-gulp.task("start", gulp.series("build", "server"));
+gulp.task('build', gulp.series('clean', 'copy', 'css', 'main', 'vendor', 'sprite', 'html'));
+gulp.task('start', gulp.series('build', 'server'));
--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,10 @@
 'use strict';
+
+import smoothscroll from 'smoothscroll-polyfill';
+import Swiper from "swiper";
+
+smoothscroll.polyfill();
+
 var subscriptionLink = document.querySelector('.button--header');
 var subscriptionAncor = document.querySelector('.subscription > a');
 
@@ -9,5 +15,58 @@ var scrollIt = function (item) {
 subscriptionLink.addEventListener('click', function (evt) {
   evt.preventDefault();
   scrollIt(subscriptionAncor);
+});
+
+window.addEventListener(`load`, () => {
+  var sliderBlock = document.querySelector('.coaches__container');
+  if (sliderBlock) {
+    var mainSlider = new Swiper('.coaches__container', {
+      loop: true,
+      navigation: {
+        nextEl: '.coaches__button--next',
+        prevEl: '.coaches__button--prev'
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1
+        },
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 30
+        },
+        1210: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 40
+        }
+      }
+    });
+  }
+
+  var slideReviewsrBlock = document.querySelector(`.feedback__wrapper`);
+  if (slideReviewsrBlock) {
+    var reviewsSlider = new Swiper(`.feedback__wrapper`, {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 57,
+      navigation: {
+        nextEl: `.feedback__button--next`,
+        prevEl: `.feedback__button--prev`
+      },
+      breakpoints: {
+        320: {
+          spaceBetween: 33
+        },
+        768: {
+          spaceBetween: 57
+        },
+        1200: {
+          spaceBetween: 80
+        }
+      }
+    });
+  }
 });
 
